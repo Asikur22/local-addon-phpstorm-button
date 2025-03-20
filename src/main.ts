9fbbcbb47adf5e834e37cb46a0eb7cbdf8febd7e
Asiqur Rahman <asikur22@gmail.com>
2025-03-20 15:06:14 +0600

    readme updated

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,1 @@
-// https://getflywheel.github.io/local-addon-api/modules/_local_main_.html
-import * as LocalMain from '@getflywheel/local/main';
-
-export default function (context) {
-}
+export default function (context) {}
